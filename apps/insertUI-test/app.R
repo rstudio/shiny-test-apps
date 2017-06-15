98f2f8936410a95c89b3b707aefb8ada936666ef
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-06-15 14:59:52 +0100

    second (unsuccessful..) test

--- apps/insertUI-test/app.R
+++ apps/insertUI-test/app.R
@@ -1,36 +1,22 @@
 library(shiny)
-library(DT)
 
 # library(shinytest)
 # recordTest("apps/insertUI-test")
 # testApp("apps/insertUI-test")
 
-# If you select rows when you run recordTest(), shinytest gets confused and even
-# though it records the test and produces a tests directory, these are only for
-# first snapshots (before clicking any rows and losing the rest of the information)
-# 
-# > recordTest("apps/insertUI-test")
-# 
-# Listening on http://127.0.0.1:7005
-# Saved test code to /Users/barbaraborges/shinyTestResults/apps/insertUI-test/tests/mytests.R
-# Saving baseline...
-# Error in session_makeRequest(self, private, endpoint, data, params, headers) :
-#   Unable to find input binding for element with id DataTables_Table_0_length
-# > testApp("apps/insertUI-test")
-# ====== Running mytests.R ======
-#   Error in session_makeRequest(self, private, endpoint, data, params, headers) :
-#   Unable to find input binding for element with id DataTables_Table_0_length
+# problem: see issue rstudio/shinytest#106
 
 ui <- fluidPage(
-  actionButton("add", "Add table", icon = icon("th")),
-  tags$div(id = "tables")
+  actionButton("add", "Add slider"),
+  tags$div(id = "sliders")
 )
 
 server <- function(input, output, session) {
   observeEvent(input$add, {
-    id <- paste0("slider-", input$add)
-    insertUI("#tables", "beforeEnd", DT::dataTableOutput(id))
-    output[[id]] <- DT::renderDataTable(head(mtcars, 3), server = FALSE)
+    containerID <- paste0("ui-", input$add)
+    sliderID <- paste0("slider-", input$add)
+    insertUI("#sliders", "beforeEnd", uiOutput(containerID))
+    output[[containerID]] <- renderUI(sliderInput(sliderID, sliderID, 0, 10, 5))
   }, ignoreInit = TRUE)
 }
 
--- apps/insertUI-test/tests/mytests.R
+++ apps/insertUI-test/tests/mytests.R
@@ -3,49 +3,13 @@ app$snapshotInit("mytests")
 
 app$snapshot()
 app$setInputs(add = "click")
-# Input '`slider-1_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_current`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_all`' was set, but doesn't have an input binding.
-# Input '`slider-1_state`' was set, but doesn't have an input binding.
-# Input '`slider-1_search`' was set, but doesn't have an input binding.
-# Input '`slider-1_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-1_row_last_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-1_row_last_clicked`' was set, but doesn't have an input binding.
+app$setInputs(`slider-1` = 5)
+app$setInputs(`slider-1` = 8)
 app$snapshot()
 app$setInputs(add = "click")
-app$setInputs(DataTables_Table_0_length = "10")
-# Input '`slider-2_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-2_rows_current`' was set, but doesn't have an input binding.
-# Input '`slider-2_rows_all`' was set, but doesn't have an input binding.
-# Input '`slider-2_state`' was set, but doesn't have an input binding.
-# Input '`slider-2_search`' was set, but doesn't have an input binding.
-# Input '`slider-2_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-2_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-2_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-2_row_last_clicked`' was set, but doesn't have an input binding.
+app$setInputs(`slider-2` = 5)
+app$setInputs(`slider-2` = 2)
 app$snapshot()
 app$setInputs(add = "click")
-app$setInputs(DataTables_Table_1_length = "10")
-# Input '`slider-3_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-3_rows_current`' was set, but doesn't have an input binding.
-# Input '`slider-3_rows_all`' was set, but doesn't have an input binding.
-# Input '`slider-3_state`' was set, but doesn't have an input binding.
-# Input '`slider-3_search`' was set, but doesn't have an input binding.
-# Input '`slider-3_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_selected`' was set, but doesn't have an input binding.
-# Input '`slider-1_row_last_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-2_cell_clicked`' was set, but doesn't have an input binding.
-# Input '`slider-2_rows_selected`' was set, but doesn't have an input binding.
+app$setInputs(`slider-3` = 5)
 app$snapshot()
-# Input '`slider-1_rows_current`' was set, but doesn't have an input binding.
-# Input '`slider-1_rows_all`' was set, but doesn't have an input binding.
-# Input '`slider-1_search`' was set, but doesn't have an input binding.
-# Input '`slider-1_search`' was set, but doesn't have an input binding.
-app$snapshot()
-
-app$snapshotCompare()
